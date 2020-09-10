aeddfd88c7f1600f7de1eb0d9b3d2a0eb5265756
Albert <albert_li@ucsb.edu>
2020-09-10 16:02:47 -0700

    Added some buttons

--- frontend/src/components/ABC.js
+++ frontend/src/components/ABC.js
@@ -5,9 +5,11 @@ export default class ABC extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			smt: null,
-			bounds: {}
+			smt: "",
+			args: [{key:"", value:""}],
 		};
+
+		this.updateSmt = this.updateSmt.bind(this);
 	}
 
 	updateSmt(value){
@@ -17,13 +19,62 @@ export default class ABC extends Component {
 	}
 
 	addBound(key,value){
-		const bounds = [...this.state.list];
-		bounds[key] = value;
+		const args = [...this.state.args];
+		args[key] = value;
 
 		this.setState({
-			bounds,
+			args,
 		});
 	}
 
-	deleteBound(
+		//deleteBound(){}
+
+	render() {
+		return(
+			<>
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<form>
+								<div class="form-group">
+									<label>SMT formula</label>
+									<input
+										type="text"
+										class="form-control"
+										name="smt"
+										onChange={this.updateSmt}
+										required/>
+								</div>
+								<div class="form-group">
+									<button>Add argument</button>{
+										this.state.args.map((k,val) =>{
+											let arg = `${k}`, v = `${val}`
+											return (
+												<div key={arg}>
+													<label htmlFor={arg}>{`Argument flag`}</label>
+													<input
+														type="text"
+														name={arg}
+														className="argument"
+													/>
+													<label htmlFor={v}>{`Value for Arguement`}</label>
+													<input
+														type="text"
+														name={arg}
+														className="argument"
+													/>
+												</div>
+											)
+										})
+									}
+								</div>
+								<button>Submit</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</>
+
+		)
+	}
 }
